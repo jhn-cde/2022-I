63ecdcaf68bea5d9ba889f14d39e4dc546948a8d
jhn-cde <140156@unsaac.edu.pe>
2022-07-03 21:21:01 -0500

    boton del

--- 03-CALCULADORAAPP/src/screens/CalculadoraScreen.tsx
+++ 03-CALCULADORAAPP/src/screens/CalculadoraScreen.tsx
@@ -1,44 +1,94 @@
+import { useState } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import { BotonCalc } from '../components/BotonCalc';
 import { styles } from '../theme/appTheme';
 
 export const CalculadoraScreen = () => {
+  const [numeroAnterior, setNumeroAnterior] = useState('0')
+  const [numero, setNumero] = useState('0')
+
+  const limpiar = () => {
+    setNumero('0')
+  }
+  const borrar = () => {
+    let nuevoNumero = numero.substring(0, numero.length-1)
+    
+    if(nuevoNumero === '') nuevoNumero = '0'
+
+    setNumero(nuevoNumero)
+  }
+  // concatenar numeros
+  const armarNumero = (numeroTexto: string) => {
+    // no aceptar doble o triple numero
+    if (numero.includes('.') && numeroTexto === '.') return
+    
+    //
+    if (numero.startsWith('0') || numero.startsWith('-0')){
+      //punto decimañ
+      if(numeroTexto !== '0' && numeroTexto !== '.' && !numero.includes('.')){
+        setNumero(numeroTexto)
+        //
+      } else if(numeroTexto === '0' && !numero.includes('.')){
+        return
+        //
+      } else{
+        setNumero(numero + numeroTexto)
+      }
+    }
+    else{
+      setNumero(numero + numeroTexto)
+    }
+  }
+  
+  // positivo / negativo
+  const positivoNegativo = () => {
+    if (numero.includes('-')) {
+      setNumero(numero.replace('-',''))
+    } else {
+      setNumero('-' + numero)
+    }
+  }
+
   return(
     <View style={styles.calculadorContainer}>
       <Text style={styles.resultadoPequenio}>
-        1,500.00
+        { numeroAnterior }
       </Text>
-      <Text style={styles.resultado}>
-        1,500.00
+      <Text
+        style={styles.resultado}
+        numberOfLines = {1}
+        adjustsFontSizeToFit = {true}
+      >
+      { numero }
       </Text>
       <View style={styles.fila}>
-        <BotonCalc text='C' color='#9b9b9b'/>
-        <BotonCalc text='+/-' color='#9b9b9b'/>
-        <BotonCalc text='%' color='#9b9b9b'/>
-        <BotonCalc text='/' color='#ff9427'/>
+        <BotonCalc text='C' color='#9b9b9b' accion={limpiar}/>
+        <BotonCalc text='+/-' color='#9b9b9b' accion={positivoNegativo}/>
+        <BotonCalc text='del' color='#9b9b9b' accion={borrar}/>
+        <BotonCalc text='/' color='#ff9427' accion={limpiar}/>
       </View>
       <View style={styles.fila}>
-        <BotonCalc text='7' color='#2d2d2d'/>
-        <BotonCalc text='8' color='#2d2d2d'/>
-        <BotonCalc text='9' color='#2d2d2d'/>
-        <BotonCalc text='x' color='#ff9427'/>
+        <BotonCalc text='7' accion={armarNumero}/>
+        <BotonCalc text='8' accion={armarNumero}/>
+        <BotonCalc text='9' accion={armarNumero}/>
+        <BotonCalc text='x' color='#ff9427' accion={limpiar}/>
       </View>
       <View style={styles.fila}>
-        <BotonCalc text='4' color='#2d2d2d'/>
-        <BotonCalc text='5' color='#2d2d2d'/>
-        <BotonCalc text='6' color='#2d2d2d'/>
-        <BotonCalc text='--' color='#ff9427'/>
+        <BotonCalc text='4' accion={armarNumero}/>
+        <BotonCalc text='5' accion={armarNumero}/>
+        <BotonCalc text='6' accion={armarNumero}/>
+        <BotonCalc text='--' color='#ff9427' accion={limpiar}/>
       </View>
       <View style={styles.fila}>
-        <BotonCalc text='1' color='#2d2d2d'/>
-        <BotonCalc text='2' color='#2d2d2d'/>
-        <BotonCalc text='3' color='#2d2d2d'/>
-        <BotonCalc text='+' color='#ff9427'/>
+        <BotonCalc text='1' accion={armarNumero}/>
+        <BotonCalc text='2' accion={armarNumero}/>
+        <BotonCalc text='3' accion={armarNumero}/>
+        <BotonCalc text='+' color='#ff9427' accion={limpiar}/>
       </View>
       <View style={styles.fila}>
-        <BotonCalc text='0' color='#2d2d2d' ancho/>
-        <BotonCalc text='.' color='#2d2d2d'/>
-        <BotonCalc text='=' color='#ff9427'/>
+        <BotonCalc text='0' accion={armarNumero} ancho/>
+        <BotonCalc text='.' accion={armarNumero}/>
+        <BotonCalc text='=' color='#ff9427' accion={limpiar}/>
       </View>
     </View>
   )
