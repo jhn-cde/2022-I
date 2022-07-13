8fa4880982394bbbe71c5898002a35a99e76b838
jhn-cde <140156@unsaac.edu.pe>
2022-07-12 23:53:38 -0500

    refactoring

--- series_app/src/selector/getCharacterByCategory.js
+++ series_app/src/selector/getCharacterByCategory.js
@@ -1,14 +1,11 @@
 import { bb_and_bcs as characters } from '../data/bb_and_bcs'
 
 export const getCharacterByCategory = (category) => {
-  const categories = category.split(',')
   const validPublisher = ['Breaking Bad', 'Better Call Saul']
 
-  for (const cat of categories) {
-    if (!validPublisher.includes(cat.trim())) {
-      throw new Error(`Categoria: ${cat} no soportada`)
-    }
+  if (!validPublisher.includes(category.trim())) {
+    throw new Error(`Categoria: ${category} no soportada`)
   }
 
-  return characters.filter(character => character.category.split(', ').includes(categories[0]))
+  return characters.filter(character => character.category.includes(category.trim()))
 }
