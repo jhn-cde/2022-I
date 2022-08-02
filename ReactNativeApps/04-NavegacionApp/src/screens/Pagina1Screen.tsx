5a8e7e6096725e53fefc0f5058fa8dbe199fd506
jwilla <140156@unsaac.edu.pe>
2022-08-02 10:02:14 -0500

    screens params

--- ReactNativeApps/04-NavegacionApp/src/screens/Pagina1Screen.tsx
+++ ReactNativeApps/04-NavegacionApp/src/screens/Pagina1Screen.tsx
@@ -2,6 +2,7 @@ import { useNavigation } from '@react-navigation/native'
 import {Text, View, Button} from 'react-native'
 import {StackScreenProps} from '@react-navigation/stack'
 import {styles} from '../theme/appTheme'
+import { TouchableOpacity } from 'react-native-gesture-handler'
 
 interface Props extends StackScreenProps<any, any>{}
 
@@ -13,6 +14,25 @@ export const Pagina1Screen = ({ navigation }: Props) => {
         title='Ir a pagina 2'
         onPress={() => navigation.navigate('Pagina2Screen')}
       />
+      <Text style={{}}>Navegar pasando argumentos</Text>
+      
+      <TouchableOpacity
+        onPress={() => navigation.navigate('PersonaScreen', {
+          id: 1,
+          name: 'Pedro'
+        })}
+      >
+        <Text>Pedro</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        onPress={() => navigation.navigate('PersonaScreen', {
+          id: 1,
+          name: 'Angela'
+        })}
+      >
+        <Text>Angela</Text>
+      </TouchableOpacity>
     </View>
   )
 }
