f5c5723ec0430794df3dd92fc4eafb5cb8158a70
jwilla <140156@unsaac.edu.pe>
2022-08-18 12:04:02 -0500

    call menuLateral

--- ReactNativeApps/04-NavegacionApp/App.tsx
+++ ReactNativeApps/04-NavegacionApp/App.tsx
@@ -3,14 +3,27 @@ import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import { MenuLateral } from './src/navigator/MenuLateral';
 import { Tabs } from './src/navigator/Tabs';
+import { AuthProvider } from './src/context/AuthContext';
 
 export default function App() {
   return (
     <>
       <NavigationContainer>
-        <Tabs />
+        <AppState>
+          {/*<MenuLateral />*/}
+          <MenuLateral />
+        </AppState>
       </NavigationContainer>
       <StatusBar style="auto" />
     </>
   );
+}
+
+
+const AppState = ({children}:any) => {
+  return (
+    <AuthProvider>
+      {children}
+    </AuthProvider>
+  )
 }
