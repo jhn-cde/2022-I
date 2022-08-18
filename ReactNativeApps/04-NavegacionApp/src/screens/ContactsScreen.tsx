6ae96862a337a7f65756f7cf27adf5318c26d06e
jwilla <140156@unsaac.edu.pe>
2022-08-18 12:09:24 -0500

    add login button

--- ReactNativeApps/04-NavegacionApp/src/screens/ContactsScreen.tsx
+++ ReactNativeApps/04-NavegacionApp/src/screens/ContactsScreen.tsx
@@ -1,10 +1,18 @@
-import { Text, View } from "react-native"
+import { useContext } from "react"
+import { Button, Text, View } from "react-native"
+import { AuthContext } from "../context/AuthContext"
 import { styles } from "../theme/appTheme"
 
 export const ContactsScreen = () => {
+  const { signIn } = useContext(AuthContext)
+
   return (
     <View style={styles.globalMargin}>
       <Text style={styles.title}>ContactsScreen</Text>
+      <Button
+        title='SignIn'
+        onPress={ signIn }
+      />
     </View>
   )
 }
