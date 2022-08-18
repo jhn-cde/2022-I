39b970310cbfe4b3a53356137e3e63edd46f7e1d
jwilla <140156@unsaac.edu.pe>
2022-08-18 12:07:56 -0500

    show auth status

--- ReactNativeApps/04-NavegacionApp/src/screens/SettingsScreen.tsx
+++ ReactNativeApps/04-NavegacionApp/src/screens/SettingsScreen.tsx
@@ -1,10 +1,16 @@
+import { useContext } from "react"
 import { Text, View } from "react-native"
 import { useSafeAreaInsets } from "react-native-safe-area-context"
-import { styles } from "../theme/appTheme"
+import { AuthContext } from "../context/AuthContext"
+import { colores, styles } from "../theme/appTheme"
+import Icon from '@expo/vector-icons/Ionicons';
 
 export const SettingsScreen = () => {
   const insets = useSafeAreaInsets()
-   return(
+
+  const { authState } = useContext( AuthContext )
+
+  return(
     <View
       style={{
         ...styles.globalMargin,
@@ -12,6 +18,10 @@ export const SettingsScreen = () => {
       }}  
     >
       <Text style={styles.title}> Settings </Text>
+      <Text>{JSON.stringify(authState, null, 4)}</Text>
+      {authState.favoriteIcon && (
+        <Icon name={authState.favoriteIcon} size={150} color={colores.primary} />
+      )}
     </View>
   )
 }
